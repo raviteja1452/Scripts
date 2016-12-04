988f81dc8d7e0149ff0f0317b4649b5928115c32
raviteja1452 <b.raviteja1452@gmail.com>
2016-12-05 00:12:02 +0530

    notifier initial code commits

--- notifier/main.py
+++ notifier/main.py
@@ -1,4 +1,5 @@
 import os,sys
+from datetime import datetime
 
 if __name__ == '__main__':
 
@@ -6,5 +7,9 @@ if __name__ == '__main__':
 	notifiers = []
 	with open('notifiers.txt') as f:
 		for line in f:
-			notifiers.append(line.split("%%%")[:3])
+			item = line.split(";")[:3];
+			print item[0]
+			item[0] = datetime.strptime(item[0].strip(), '%d/%m/%Y %I:%M:%S %p')
+			print item[0]
+			notifiers.append(item)
 	print notifiers
