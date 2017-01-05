0980ee9b7e87e4083e15507bb466077cc10b9db5
raviteja1452 <b.raviteja1452@gmail.com>
2017-01-05 12:06:50 +0530

    notifier

--- notifier/main.py
+++ notifier/main.py
@@ -1,15 +1,19 @@
 import os,sys
 from datetime import datetime
 
-if __name__ == '__main__':
-
-	#List of Notifiers
+def readNotifications():
 	notifiers = []
 	with open('notifiers.txt') as f:
 		for line in f:
-			item = line.split(";")[:3];
-			print item[0]
+			item = line.split(";")[:3]
 			item[0] = datetime.strptime(item[0].strip(), '%d/%m/%Y %I:%M:%S %p')
-			print item[0]
 			notifiers.append(item)
-	print notifiers
+	notifiers.sort(key=lambda x: x[0])
+	return notifiers
+def printNotification(subject,message):
+	return TRUE
+if __name__ == '__main__':
+
+	#List of Notifiers
+	notifiers = readNotifications()
+	printNotification('Ravi','Have a good time')
